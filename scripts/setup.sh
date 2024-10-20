eea2210d5584cb0eba876719518bc26ccc9963b9
Alvaro Lopez <al7aro@gmail.com>
2024-10-20 18:07:03 +0200

    FIX: Linux script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/..
 
 git submodule update --init
-mkdir build 2>/dev/null
-cmake -B build $parent_path/../
+cmake -B $parent_path/build $parent_path
