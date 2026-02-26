506186223f98df63b1964cc0408ef4a2f308c567
Alvaro <al7aro@gmail.com>
2026-02-26 21:31:06 +0100

    Init Commit

---
+++ scripts/setup.bat
@@ -0,0 +1,17 @@
+@echo off
+cd /d "%~dp0"
+cd ..
+set VENDOR_DIR=vendor
+if not exist %VENDOR_DIR% mkdir %VENDOR_DIR%
+
+@REM FETCH GLAD
+set REPO_URL=git@github.com:al7aro/glad.git
+git clone %REPO_URL% %VENDOR_DIR%\glad
+
+@REM FETCH STB
+set REPO_URL=git@github.com:al7aro/stb.git
+git clone %REPO_URL% %VENDOR_DIR%\stb
+
+@REM FETCH GLFW
+set REPO_URL=https://github.com/glfw/glfw.git
+git clone %REPO_URL% -b 3.3-stable %VENDOR_DIR%\glfw
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Also try Hazel\n");
+    return (0);
+}
